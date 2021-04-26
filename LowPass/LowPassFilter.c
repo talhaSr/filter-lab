2ef9f3bf84e35ca0bfdae955b31f335221bcae92
Talha Sarı <talhasari4343@outlook.com.tr>
2021-04-26 16:18:07 +0300

    Low Pass Filter added
    Low Pass Filter and Test Script added

---
+++ LowPass/LowPassFilter.c
@@ -0,0 +1,55 @@
+/**
+ * @file LowPassFilter.c
+ * @author Talha Sarı (talha.sari@outlook.com.tr)
+ * @brief A Low-pass Filter Implementation in C
+ * @version v1.0
+ * @date 2021-04-26
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+
+#include "LowPassFilter.h"
+
+void LowPassFilter_Init(LowPassFilter *filter, float cutoffFreq, float samplePeriod)
+{
+    /* Init Input Buffer */
+    filter->input = 0.0f;
+
+    /* Calculate Alpha Coefficient */
+    filter->alpha = __ALPHA(cutoffFreq, __FS(samplePeriod));
+
+    /* Clear Filter Output */
+    filter->output[0] = 0.0f;
+    filter->output[1] = 0.0f;
+}
+
+float LowPassFilter_Update(LowPassFilter *filter, float input)
+{
+    /* Update Variables */
+    filter->output[1] = filter->input;
+    filter->input = input;
+
+    /* Calculate Output */
+    filter->output[0] = (filter->alpha * filter->input) + ((1.0f - filter->alpha) * filter->output[1]);
+
+    return filter->output[0];
+}
---
+++ LowPass/LowPassFilter.h
@@ -0,0 +1,32 @@
+/**
+ * @file LowPassFilter.h
+ * @author Talha Sarı (talha.sari@outlook.com.tr)
+ * @brief A Low-pass Filter Implementation in C
+ * @version v1.0
+ * @date 2021-04-26
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef LOW_PASS_FILTER_H
+#define LOW_PASS_FILTER_H
+
+/* Macros */
+#define PI              3.141592
+#define __FS(T)         (1.0F / T)
+#define __ALPHA(fc, T)  ((2.0F * PI * T * fc) / ((2.0F * PI * T * fc) + 1.0F))
+
+/* Filter HandleTypeDef */
+typedef struct LowPassFilter
+{
+    float input;
+    float alpha;
+    float output[2];
+} LowPassFilter;
+
+/* Functions */
+void LowPassFilter_Init(LowPassFilter *filter, float cutoffFreq, float samplePeriod);
+float LowPassFilter_Update(LowPassFilter *filter, float input);
+
+#endif /* LOW_PASS_FILTER_H */
